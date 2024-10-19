7ee96f528a663a8cf0f83731a46ba745cd95153e
gerson-msj <gerson.msj@gmail.com>
2024-10-19 18:38:31 -0300

    Update

---
+++ src/web/app/scripts/app.js
@@ -0,0 +1,9 @@
+define("app", ["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.default = main;
+    function main() {
+        alert("Teste 1");
+    }
+});
+//# sourceMappingURL=app.js.map
